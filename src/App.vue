1ad8f541d4c097176e56c4dabce7311a20a417b1
shikai340826 <shikai340826@sina.com>
2024-09-05 14:28:01 +0800

    第一次提交测试

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>{{ sum }}</h2>
+    <button @click="increment">点击加一</button>
+  </div>
+</template>
+
+<script setup lang='ts'>
+  import { ref, reactive } from 'vue'
+  let sum = ref(0);
+  function increment() {
+    sum.value += 1;
+  }
+</script>
+
+<style scoped>
+  h2{
+    color: green;
+  }
+</style>
